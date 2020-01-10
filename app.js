ac980aaed8575f8071ea4b13925251173a41bdda
ericpkatz <ericpkatz@gmail.com>
2020-01-10 00:35:59 -0500

    commit

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const promises = urls.map( url => {
 });
 
 const renderData = (endpoint, data, itemRenderer)=> {
+  console.log(data);
   const div = document.querySelector(`#${ endpoint }`);
   let html = data.results.map( item => {
     return `
@@ -17,8 +18,31 @@ const renderData = (endpoint, data, itemRenderer)=> {
       </li>
     `;
   }).join('');
-  html = `<h2>${endpoint}</h2><ul>${html}</ul>`;
+  html = `<h2>${endpoint}</h2><input /><div>Viewing <span class='count'>${ data.results.length}</span> of ${ data.count }</div><ul>${html}</ul>`;
+
   div.innerHTML = html;
+  setUpSearch(div);
+};
+
+const setUpSearch = (div)=> {
+  const input = div.querySelector('input');
+  const ul = div.querySelector('ul');
+  const counter = div.querySelector('.count');
+  input.addEventListener('keyup', (ev)=> {
+    const search = ev.target.value;
+    const lis = [...ul.children];
+    let count = 0;
+    lis.forEach( li => {
+      if(li.innerHTML.toLowerCase().includes(search.toLowerCase())){
+        li.classList.remove('hide');
+        count++;
+      }
+      else {
+        li.classList.add('hide');
+      }
+    });
+    counter.innerHTML = count;
+  });
 };
 
 const renderPeople = (people)=> {
